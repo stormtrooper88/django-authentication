ba122d82178ad42408ae8dd4dd735e04723f4049
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-05-19 09:31:46 +0000

    added authorisation video and content to code, this in views.py file

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 
@@ -8,6 +9,7 @@ def index(request):
     return render(request, 'index.html')
 
 
+@login_required
 def logout(request):
     """log the user out"""
     auth.logout(request)
@@ -16,6 +18,8 @@ def logout(request):
 
 def login(request):
     """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)  
 
@@ -26,6 +30,7 @@ def login(request):
         if user:
             auth.login(user=user, request=request)
             messages.success(request, "You have successfully logged in!")
+            return redirect(reverse('index'))
 
         else:
             login_form.add_error(None, "Your username or password is incorrect")
