8c9ae9ae9b035f8a06b726eac1a45f5cf474c122
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-05-19 09:24:11 +0000

    Added templates, base html file, and created blocks

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from accounts.forms import UserLoginForm
 
 
 def index(request):
@@ -13,7 +14,22 @@ def logout(request):
     messages.success(request, "You have successfully been logged out!")
     return redirect(reverse('index'))
 
-
 def login(request):
     """Return a login page"""
-    return render(request, 'login.html')
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)  
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'], 
+                                    password=request.POST['password'])
+
+        if user:
+            auth.login(user=user, request=request)
+            messages.success(request, "You have successfully logged in!")
+
+        else:
+            login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+
+    return render(request, 'login.html', {'login_form': login_form})
